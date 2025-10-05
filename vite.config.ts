37c5a0ce8d541714a9d3f42db40dfe0bbe3e41ef
Kalle1231233 <nemo.pauli@icloud.com>
2025-10-05 21:45:01 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/mein_app5-master/",
-  build: {
-    outDir: "docs",
-  },
+  build: { outDir: "docs" },
   plugins: [react()],
 });
